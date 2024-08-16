f68425e16993527840531e3756e933edf512b84e
saranya-k200 <saranya101105@gmail.com>
2024-08-16 20:28:09 +0530

    welcome email created

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-use App\http\Controllers\HomeController;
+use App\Http\Controllers\WelcomeEmailController;
 
-Route::get('/thirumal',[HomeController::class, 'sendEmailManually'])->name('home.sendEmailManually');
+Route::get('/send-welcome-email', [WelcomeEmailController::class, 'index']);
