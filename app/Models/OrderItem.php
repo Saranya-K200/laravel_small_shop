977a4e252439a702c6a652497f2036a607b533cf
saranya-k200 <saranya101105@gmail.com>
2024-08-17 20:17:11 +0530

    Order Item model and migration

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'qty',
+        'unit_price',
+        'amount',
+        'discount'
+    ];
+
+    public function order()
+    {
+        return $this->belongTo(Order::class, 'order_id');
+    }
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+}
