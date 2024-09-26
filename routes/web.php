b92eef05aca1596cdcc3ef501fcff1ea35915436
saranya-k200 <saranya101105@gmail.com>
2024-09-26 19:05:32 +0530

    laravel partial setup

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\AuthController;
+
+Route::get('/forget_password',[AuthController::class, 'forget_password'])->name('home.forget_password');
+
 use App\Http\Controllers\WelcomeEmailController;
 
 Route::get('/send-welcome-email', [WelcomeEmailController::class, 'index']);
